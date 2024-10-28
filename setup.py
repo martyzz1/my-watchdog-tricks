891966972c56521fee0062d97f4b683910c6c289
Martin Moss <martyzz1@gmail.com>
2024-10-28 23:33:20 +0000

    Bump version: 2.1.1 → 2.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="my-watchdog-tricks",
-    version="2.1.1",
+    version="2.1.2",
     license="MIT",
     author="Martin Moss",
     author_email="martyzz1@github.com",
